924b0d1fdf3822937903c051a767ca2da92a282d
Furquan Anwer <44668513+FurquanAnwer@users.noreply.github.com>
2024-05-11 21:22:01 +0530

    first commit for ant design website

---
+++ src/components/PageContent.js
@@ -0,0 +1,11 @@
+// import AppRoutes from "Route"
+import AppRoutes from "./Route";
+
+function PageContent(){
+    return (
+      <div className='pageContent'>
+        <AppRoutes/>
+      </div>
+    );
+  }
+export default PageContent;  
